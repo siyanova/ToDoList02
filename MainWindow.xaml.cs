0c6ffc7355b7f15d22dc57aa5496a7c8d97e8f3b
fuck <nw.finn@yandex.ru>
2024-04-04 14:28:25 +0300

    dsgdg

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Dynamic;
 using System.Linq;
 using System.Text;
@@ -21,29 +22,51 @@ namespace ToDoList02
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(SqlConnection conn, string id)
         {
             InitializeComponent();
+            this.conn = conn;
+            this.id = id;
+            WriteNickname();
+            WriteCategories();
+           
         }
-        public Grid CreateButton(string idIcon, string nameTask)
+        private SqlConnection conn;
+        private string id;
+        private void WriteNickname()
+        {
+            SqlCommand comm = new SqlCommand("select FIO from [User] where idUser = "+id,conn);
+            SqlDataReader reader = comm.ExecuteReader();
+            reader.Read();
+            NickName.Text = reader[0].ToString();
+            reader.Close();
+        }
+        private void WriteCategories()
+        {
+            SqlCommand sqlCommand = new SqlCommand("select name, idIcon from [Section] where idUser= " + id, conn);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                ListSection.Items.Insert(0,CreateButton(reader[1].ToString(), reader[0].ToString()));
+            }
+            reader.Close();
+        } 
+        public Grid CreateButton(string idIcon,string NameTask)
         {
             Grid grid = new Grid();
-            ColumnDefinition columnDefinition = new ColumnDefinition();
-            grid.ColumnDefinitions.Add(columnDefinition);
-
-            ColumnDefinition columnDefinition1 = new ColumnDefinition();
-            columnDefinition1.Width = new GridLength(4, GridUnitType.Star);
-            grid.ColumnDefinitions.Add(columnDefinition1);
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
 
             Image image = new Image();
-            image.Source = new BitmapImage(new Uri("/image/fon/" + idIcon + ".jpg", UriKind.Relative));
-            image.Width = 40;
-            image.Height = 40;
+            image.Source = new BitmapImage(new Uri("image/icon/"+idIcon+".png", UriKind.Relative));
+            image.Width = 48;
+            image.Height = 43;
             Grid.SetColumn(image, 0);
             grid.Children.Add(image);
 
             TextBlock textBlock = new TextBlock();
-            textBlock.Text = nameTask;
+            textBlock.Text = NameTask;
+            textBlock.FontSize = 14;
             textBlock.TextAlignment = TextAlignment.Center;
             textBlock.VerticalAlignment = VerticalAlignment.Center;
             Grid.SetColumn(textBlock, 1);
@@ -56,7 +79,23 @@ namespace ToDoList02
             Section section = new Section();
             addTask window = new addTask(section);
             window.ShowDialog();
+            ListSection.Items.Insert(0,CreateButton(section.idIcon, section.name));
+        }
 
+        private void ListSection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Stack.Children.Clear();
+            string a = ((TextBlock)((Grid)ListSection.SelectedItem).Children[1]).Text;
+            SqlCommand comm = new SqlCommand($"select description from Task where idSection = (select id from Section where name = '"+a+"');", conn);
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                CheckBox checkbox = new CheckBox();
+                checkbox.Content = reader[0];
+                checkbox.FontSize = 15;
+                Stack.Children.Add(checkbox);
+            }
+            reader.Close();
         }
     }
 }
---
+++ regist.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList02
+{
+    /// <summary>
+    /// Логика взаимодействия для regist.xaml
+    /// </summary>
+    public partial class regist : Window
+    {
+        private SqlConnection conn;
+        public regist()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(@"Data Source = 3205EC01; Initial Catalog = ToDoList2; Integrated Security=SSPI;");
+            conn.Open();
+
+        }
+
+        private void entry_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SqlCommand cmd = new SqlCommand("select idUser from [User] where login = '" + UserNameLogin.Text + "' and password = '" + UserNamePassword.Text + "';", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                var id = reader[0].ToString();
+                reader.Close();
+                MainWindow mainWindow = new MainWindow(conn,id);
+                mainWindow.Show();
+                this.Close();
+            }
+
+        }
+    }
+}
